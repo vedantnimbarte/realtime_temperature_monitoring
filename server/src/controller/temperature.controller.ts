240d5f14c3bf8c1409bced87a4d6a4a792952723
Vedant Nimbarte <vedantnimbarte7@gmail.com>
2021-09-04 16:27:24 +0530

    added temperaturegethandler to temperature controller

--- server/src/controller/temperature.controller.ts
+++ server/src/controller/temperature.controller.ts
@@ -1,11 +1,27 @@
 import { Request, Response } from "express";
-import { insert } from "../service/temperature.service";
+import { insert, get } from "../service/temperature.service";
 
 export async function temperatureInsertHandler(req: Request, res: Response) {
   try {
-    const result = await insert(req.body);
-    return res.status(200);
+    await insert(req.body);
+    return res.sendStatus(200);
   } catch (err) {
     return res.status(502).send(err);
+  }
+}
+
+export async function temperatureGetHandler(req: Request, res: Response) {
+  try {
+    let result = await get(req.body);
+    let result_length = Object.keys(result).length;
+    if (result_length > 0) {
+      return res.status(200).json({ message: result });
+    } else {
+      return res
+        .status(404)
+        .json({ message: "No data available with this device id" });
+    }
+  } catch (err) {
+    return res.send(502).send(err);
   }
 }
