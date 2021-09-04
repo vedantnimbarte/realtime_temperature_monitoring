bdf4e7153b174363b8f92b8066471bba0f22678e
Vedant Nimbarte <vedantnimbarte7@gmail.com>
2021-09-04 16:30:27 +0530

    added get function to get data from db

--- server/src/service/temperature.service.ts
+++ server/src/service/temperature.service.ts
@@ -1,5 +1,6 @@
-import { getConnection } from "typeorm";
+import { getConnection, getRepository } from "typeorm";
 import { Temperature } from "../entity/temperature.entity";
+import log from "../logger";
 
 export async function insert(input) {
   const result = await getConnection()
@@ -12,7 +13,31 @@ export async function insert(input) {
       temp2: input.temp2,
       date: input.date,
       time: input.time,
+      min_temp: input.min_temp,
+      max_temp: input.max_temp,
+      min_temp_status: input.min_temp_status,
+      max_temp_status: input.max_temp_status,
     })
     .execute();
+  return result;
+}
+
+export async function get(input) {
+  const repository = await getRepository(Temperature);
+  const result = await repository.find({
+    select: [
+      "device_id",
+      "temp1",
+      "temp2",
+      "date",
+      "time",
+      "min_temp",
+      "max_temp",
+      "min_temp_status",
+      "max_temp_status",
+      "material_type",
+    ],
+    where: { device_id: input.device_id },
+  });
   return result;
 }
