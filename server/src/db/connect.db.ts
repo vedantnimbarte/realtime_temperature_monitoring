fa2039437e0d0e874d07217a61aceb14c1cb9d16
Vedant Nimbarte <vedantnimbarte7@gmail.com>
2021-09-04 12:30:24 +0530

    added database connection file

---
+++ server/src/db/connect.db.ts
@@ -0,0 +1,35 @@
+import "reflect-metadata";
+import * as config from "config";
+import { createConnection } from "typeorm";
+import { Users } from "../entity/users.entity";
+import log from "../logger";
+import { Temperature } from "../entity/temperature";
+
+export default function connect() {
+  const db_host = config.get("dbHost") as string;
+  const db_user = config.get("dbUser") as string;
+  const db_password = config.get("dbPassword") as string;
+  const db_port = config.get("dbPort") as number;
+  const db_name = config.get("dbName") as string;
+
+  log.info("Connecting to database...");
+
+  return createConnection({
+    type: "mysql",
+    host: db_host,
+    port: db_port,
+    username: db_user,
+    password: db_password,
+    database: db_name,
+    entities: [Temperature],
+    synchronize: true,
+    logging: false,
+  })
+    .then(() => {
+      log.info(`Connected to database...`);
+    })
+    .catch((error) => {
+      log.error(`[DATABASE ERROR]: ${error}`);
+      process.exit(1);
+    });
+}
