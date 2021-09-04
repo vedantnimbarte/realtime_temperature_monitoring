787c1d07b6a38d5df9ca5ff56a6c3e9a0578a845
Vedant Nimbarte <vedantnimbarte7@gmail.com>
2021-09-04 13:19:05 +0530

    added /api/temperature post route to insert temperature to database

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,5 +1,10 @@
 import { Express, Request, Response } from "express";
+import { temperatureInsertHandler } from "./controller/temperature.controller";
 
 export default function (app: Express) {
-  app.get("/healthcheck", (req: Request, res: Response) => res.sendStatus(200));
+  app.get("/api/healthcheck", (req: Request, res: Response) =>
+    res.sendStatus(200)
+  );
+
+  app.post("/api/temperature", temperatureInsertHandler);
 }
