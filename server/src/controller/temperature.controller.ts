e51bcba4c79f309be9375a08f10176a4e853b945
Vedant Nimbarte <vedantnimbarte7@gmail.com>
2021-09-07 14:08:50 +0530

    added new function for getting data according to error code in desc order

--- server/src/controller/temperature.controller.ts
+++ server/src/controller/temperature.controller.ts
@@ -1,5 +1,12 @@
 import { Request, Response } from "express";
-import { insert, get } from "../service/temperature.service";
+import log from "../logger";
+import {
+  insert,
+  get,
+  getMinTemp,
+  getMaxTemp,
+  getTempStatus,
+} from "../service/temperature.service";
 
 export async function temperatureInsertHandler(req: Request, res: Response) {
   try {
@@ -7,13 +14,13 @@ export async function temperatureInsertHandler(req: Request, res: Response) {
     return res.status(200).json({
       success: 1,
       errorMsg: 0,
-      result: [{ message: "Data inserted successfully" }],
+      temperatureData: [req.body],
     });
   } catch (err) {
     return res.status(502).json({
       success: 0,
       errorMsg: 1,
-      result: [{ message: err }],
+      temperatureData: [],
     });
   }
 }
@@ -22,17 +29,87 @@ export async function temperatureGetHandler(req: Request, res: Response) {
   try {
     let result = await get(req.body);
     let result_length = Object.keys(result).length;
+    log.info(result);
     if (result_length > 0) {
       return res.status(200).json({
         success: 1,
         errorMsg: 0,
-        result: [{ ...result }],
+        temperatureData: [...result],
       });
     } else {
       return res.status(404).json({
+        success: 0,
+        errorMsg: 1,
+        temperatureData: [],
+      });
+    }
+  } catch (err) {
+    return res.send(502).send(err);
+  }
+}
+
+export async function temperatureGetStatusHandler(req: Request, res: Response) {
+  try {
+    let result = await getTempStatus(req.body);
+    let result_length = Object.keys(result).length;
+    log.info(result);
+    if (result_length > 0) {
+      return res.status(200).json({
         success: 1,
         errorMsg: 0,
-        result: [{ message: "No device data available with this id" }],
+        temperatureData: [...result],
+      });
+    } else {
+      return res.status(404).json({
+        success: 0,
+        errorMsg: 1,
+        temperatureData: [],
+      });
+    }
+  } catch (err) {
+    return res.send(502).send(err);
+  }
+}
+
+export async function temperatureMinGetHandler(req: Request, res: Response) {
+  try {
+    let result = await getMinTemp(req.body);
+    let result_length = Object.keys(result).length;
+    log.info(result);
+    if (result_length > 0) {
+      return res.status(200).json({
+        success: 1,
+        errorMsg: 0,
+        temperatureData: [...result],
+      });
+    } else {
+      return res.status(404).json({
+        success: 0,
+        errorMsg: 1,
+        temperatureData: [],
+      });
+    }
+  } catch (err) {
+    return res.send(502).send(err);
+  }
+}
+
+export async function temperatureMaxGetHandler(req: Request, res: Response) {
+  try {
+    let result = await getMaxTemp(req.body);
+    let result_length = Object.keys(result).length;
+    log.info(result);
+    if (result_length > 0) {
+      return res.status(200).json({
+        success: 1,
+        errorMsg: 0,
+        temperatureData: [...result],
+      });
+    } else {
+      return res.status(404).json({
+        success: 0,
+        errorMsg: 1,
+        result: [],
       });
     }
   } catch (err) {
