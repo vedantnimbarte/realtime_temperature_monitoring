0e76d0da09993dde815d118887790d5454472da7
Vedant Nimbarte <vedantnimbarte7@gmail.com>
2021-09-07 14:10:12 +0530

    added new function for getting data in desc order accroding to error_code, device_id and date

--- server/src/service/temperature.service.ts
+++ server/src/service/temperature.service.ts
@@ -17,6 +17,7 @@ export async function insert(input) {
       max_temp: input.max_temp,
       min_temp_status: input.min_temp_status,
       max_temp_status: input.max_temp_status,
+      material_type: input.material_type,
     })
     .execute();
   return result;
@@ -36,8 +37,83 @@ export async function get(input) {
       "min_temp_status",
       "max_temp_status",
       "material_type",
+      "error_code",
     ],
     where: { device_id: input.device_id },
+  });
+  return result;
+}
+
+export async function getTempStatus(input) {
+  const repository = await getRepository(Temperature);
+  const result = await repository.find({
+    select: [
+      "device_id",
+      "temp1",
+      "temp2",
+      "date",
+      "time",
+      "min_temp",
+      "max_temp",
+      "min_temp_status",
+      "max_temp_status",
+      "material_type",
+      "error_code",
+    ],
+    where: {
+      device_id: input.device_id,
+      date: input.date,
+      error_code: input.error_code,
+    },
+    order: {
+      date: "DESC",
+    },
+  });
+  return result;
+}
+
+export async function getMinTemp(input) {
+  const repository = await getRepository(Temperature);
+  const result = await repository.find({
+    select: [
+      "device_id",
+      "temp1",
+      "temp2",
+      "date",
+      "time",
+      "min_temp",
+      "max_temp",
+      "min_temp_status",
+      "max_temp_status",
+      "material_type",
+    ],
+    where: {
+      device_id: input.device_id,
+      min_temp_status: input.min_temp_status,
+    },
+  });
+  return result;
+}
+
+export async function getMaxTemp(input) {
+  const repository = await getRepository(Temperature);
+  const result = await repository.find({
+    select: [
+      "device_id",
+      "temp1",
+      "temp2",
+      "date",
+      "time",
+      "min_temp",
+      "max_temp",
+      "min_temp_status",
+      "max_temp_status",
+      "material_type",
+    ],
+    where: {
+      device_id: input.device_id,
+      max_temp_status: input.max_temp_status,
+    },
   });
   return result;
 }
