921dc80c75cb0723650e58305dab401737643665
Vedant Nimbarte <vedantnimbarte7@gmail.com>
2021-09-08 15:23:21 +0530

    importing top navigation from components and added to dashboard

---
+++ client/src/Pages/Dashboard.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import TopNavBar from "../components/TopNavBar.component";
+
+export default class Dashboard extends Component {
+  render() {
+    return (
+      <div>
+        <TopNavBar />
+      </div>
+    );
+  }
+}
