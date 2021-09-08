ee4584e57d4389915e4389d4514fde6e950a3e07
Vedant Nimbarte <vedantnimbarte7@gmail.com>
2021-09-08 15:21:23 +0530

    fixed copyright component import

--- client/src/Pages/SignUp.js
+++ client/src/Pages/SignUp.js
@@ -10,7 +10,7 @@ import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
 import Typography from "@material-ui/core/Typography";
 import { makeStyles } from "@material-ui/core/styles";
 import Container from "@material-ui/core/Container";
-import Copyright from "../components/Copyright";
+import Copyright from "../components/Copyright.component";
 
 const useStyles = makeStyles((theme) => ({
   paper: {
