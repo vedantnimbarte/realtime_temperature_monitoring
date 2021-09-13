cfb49686b10d0451c8a4ccd6ea369d1fc9b078bb
Vedant Nimbarte <vedantnimbarte7@gmail.com>
2021-09-13 13:27:48 +0530

    created user controller and added add update select functionality to user controller

---
+++ server/src/controller/users.controller.ts
@@ -0,0 +1,62 @@
+import { Request, Response } from "express";
+import { addUser, getAllUsers, updateUser } from "../service/users.service";
+
+export async function AddUserHandler(req: Request, res: Response) {
+  try {
+    await addUser(req.body);
+    return res.status(200).json({
+      success: "1",
+      errorMsg: "0",
+      temperatureData: [req.body],
+    });
+  } catch (err) {
+    return res.status(502).json({
+      success: "0",
+      errorMsg: err,
+      temperatureData: null,
+    });
+  }
+}
+
+export async function updateUserHandler(req: Request, res: Response) {
+  try {
+    await updateUser(req.body);
+    return res.status(200).json({
+      success: "1",
+      errorMsg: "0",
+      temperatureData: [req.body],
+    });
+  } catch (err) {
+    return res.status(502).json({
+      success: "0",
+      errorMsg: err,
+      temperatureData: null,
+    });
+  }
+}
+
+export async function getUsersHandler(req: Request, res: Response) {
+  try {
+    let result = await getAllUsers(req.body);
+    let result_length = Object.keys(result).length;
+    if (result_length > 0) {
+      return res.status(200).json({
+        success: "1",
+        errorMsg: "0",
+        temperatureData: [...result],
+      });
+    } else {
+      return res.status(200).json({
+        success: "0",
+        errorMsg: "No Data Available",
+        temperatureData: null,
+      });
+    }
+  } catch (err) {
+    return res.status(502).json({
+      success: "0",
+      errorMsg: err,
+      temperatureData: null,
+    });
+  }
+}
