4500c30d8650912695d7dfa8e093aa770f34a13d
Vedant Nimbarte <vedantnimbarte7@gmail.com>
2021-09-13 12:50:25 +0530

    added cors functionality to the server

--- server/src/server.ts
+++ server/src/server.ts
@@ -4,14 +4,20 @@ import * as config from "config";
 import log from "./logger";
 import connect from "./db/connect.db";
 import routes from "./routes";
+const cors = require("cors");
 
 const port = config.get("port") as number;
 const host = config.get("host") as string;
 
 const app: express.Express = express();
 
+const corsOptions = {
+  origin: "*",
+};
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cors(corsOptions));
 
 app.listen(port, host, () => {
   log.info(`Server listening at http://${host}:${port}`);
