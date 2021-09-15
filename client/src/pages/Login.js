b6fa6b6b3b69415510030f26a886493888ae2dae
Vedant Nimbarte <vedantnimbarte7@gmail.com>
2021-09-15 16:16:14 +0530

    added login functionality

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -12,7 +12,7 @@ import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
 import Typography from "@material-ui/core/Typography";
 import { makeStyles } from "@material-ui/styles";
 import Container from "@material-ui/core/Container";
-import { Link as RouterLink } from "react-router-dom";
+import { Link as RouterLink, useNavigate } from "react-router-dom";
 
 function Copyright() {
   return (
@@ -49,6 +49,36 @@ const useStyles = makeStyles((theme) => ({
 
 export default function SignIn() {
   const classes = useStyles();
+  const navigate = useNavigate();
+  const [email, setEmail] = React.useState();
+  const [password, setPassword] = React.useState();
+  const [error, setError] = React.useState();
+  const [success, setSuccess] = React.useState();
+
+  const getDataFromApi = async () => {
+    setError();
+    setSuccess();
+    const response = await fetch("http://localhost:8000/api/user/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ email: email, password: password }),
+    });
+    const result = await response.json();
+    if (result.success == "1") {
+      navigate("/app/dashboard", { replace: true });
+      setSuccess(1);
+    } else {
+      setSuccess(0);
+      setTimeout(() => {
+        setSuccess();
+      }, 4000);
+      setError(
+        "User not available with this credentails, Please check your email and password"
+      );
+    }
+  };
 
   return (
     <Container component="main" maxWidth="xs">
@@ -61,57 +91,58 @@ export default function SignIn() {
         <Typography component="h1" variant="h5">
           Sign in
         </Typography>
-        <form className={classes.form} noValidate>
-          <TextField
-            variant="outlined"
-            margin="normal"
-            required
-            fullWidth
-            id="email"
-            label="Email Address"
-            name="email"
-            autoComplete="email"
-            autoFocus
-          />
-          <TextField
-            variant="outlined"
-            margin="normal"
-            required
-            fullWidth
-            name="password"
-            label="Password"
-            type="password"
-            id="password"
-            autoComplete="current-password"
-          />
-          <FormControlLabel
-            control={<Checkbox value="remember" color="primary" />}
-            label="Remember me"
-          />
-          <RouterLink to="/app/dashboard">
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              color="primary"
-              className={classes.submit}
-            >
-              Sign In
-            </Button>
-          </RouterLink>
-          <Grid container>
-            <Grid item xs>
-              <Link href="#" variant="body2">
-                Forgot password?
-              </Link>
-            </Grid>
-            <Grid item>
-              <Link href="#" variant="body2">
-                {"Don't have an account? Sign Up"}
-              </Link>
-            </Grid>
+        {success == 0 ? (
+          <Typography component="h1" variant="h5" sx={{ color: "red" }}>
+            {error}
+          </Typography>
+        ) : (
+          ""
+        )}
+        {/* <form className={classes.form} noValidate> */}
+        <TextField
+          variant="outlined"
+          margin="normal"
+          required
+          fullWidth
+          id="email"
+          label="Email Address"
+          name="email"
+          autoComplete="email"
+          value={email}
+          onChange={(event) => setEmail(event.target.value)}
+          autoFocus
+        />
+        <TextField
+          variant="outlined"
+          margin="normal"
+          required
+          fullWidth
+          name="password"
+          label="Password"
+          type="password"
+          id="password"
+          value={password}
+          onChange={(event) => setPassword(event.target.value)}
+          autoComplete="current-password"
+        />
+        <Button
+          type="submit"
+          fullWidth
+          variant="contained"
+          color="primary"
+          className={classes.submit}
+          onClick={() => getDataFromApi()}
+        >
+          Sign In
+        </Button>
+        <Grid container>
+          <Grid item>
+            <Link href="register" variant="body2">
+              {"Don't have an account? Sign Up"}
+            </Link>
           </Grid>
-        </form>
+        </Grid>
+        {/* </form> */}
       </div>
       <Box mt={8}>
         <Copyright />
