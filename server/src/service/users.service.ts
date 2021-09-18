b1a2120859c744b32608b2dbedc0e6c944f0aef5
Vedant Nimbarte <vedantnimbarte7@gmail.com>
2021-09-18 16:58:16 +0530

    added email and mobile validation to add user service

--- server/src/service/users.service.ts
+++ server/src/service/users.service.ts
@@ -1,6 +1,28 @@
-import { response } from "express";
 import { getRepository, getConnection } from "typeorm";
 import { Users } from "../entity/Users.entity";
+
+export async function emailVerify(input) {
+  const repository = await getRepository(Users);
+  const result = await repository.find({
+    where: {
+      email: input.email,
+    },
+  });
+  console.log(result);
+  let result_length = Object.keys(result).length;
+  return result_length > 0 ? true : false;
+}
+
+export async function mobileVerify(input) {
+  const repository = await getRepository(Users);
+  const result = await repository.find({
+    where: {
+      mobile_no: input.mobile_no,
+    },
+  });
+  let result_length = Object.keys(result).length;
+  return result_length > 0 ? true : false;
+}
 
 export async function addUser(input) {
   const result = await getConnection()
@@ -15,7 +37,7 @@ export async function addUser(input) {
       job_title: input.job_title,
     })
     .execute();
-  return result;
+  return [result, false];
 }
 
 export async function updateUser(input) {
@@ -30,13 +52,13 @@ export async function updateUser(input) {
 
 export async function getAllUsers(input) {
   const repository = getRepository(Users);
-  const result = repository.find();
+  const result = await repository.find();
   return result;
 }
 
 export async function login(input) {
   const repository = getRepository(Users);
-  const result = repository.find({
+  const result = await repository.find({
     where: {
       email: input.email,
       password: input.password,
