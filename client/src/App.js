8c1e220bb5bc20b420164f2a7fe8a47c65444f47
Vedant Nimbarte <vedantnimbarte7@gmail.com>
2021-09-18 18:11:52 +0530

    removed unused line

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,9 @@
-import 'react-perfect-scrollbar/dist/css/styles.css';
-import { useRoutes } from 'react-router-dom';
-import { ThemeProvider, StyledEngineProvider } from '@material-ui/core';
-import GlobalStyles from './components/GlobalStyles';
-import theme from './theme';
-import routes from './routes';
+import "react-perfect-scrollbar/dist/css/styles.css";
+import { useRoutes } from "react-router-dom";
+import { ThemeProvider, StyledEngineProvider } from "@material-ui/core";
+import GlobalStyles from "./components/GlobalStyles";
+import theme from "./theme";
+import routes from "./routes";
 
 const App = () => {
   const content = useRoutes(routes);
