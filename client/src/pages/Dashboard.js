b7c41488009c8deeb048d22da9b25f1b86094f3a
Vedant Nimbarte <vedantnimbarte7@gmail.com>
2021-09-23 16:56:12 +0530

    checking if cookie is present or not for authentication

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -5,37 +5,50 @@ import Budget from "../components/dashboard/Budget";
 import MaxTemperatureHits from "../components/dashboard/MaxTemperatureHits";
 import MinTemperatureHits from "../components/dashboard/MinTemperatureHits.js";
 import TotalTempHits from "../components/dashboard/TotalTempHits";
+import { useNavigate } from "react-router-dom";
+import { useCookies } from "react-cookie";
 
-const Dashboard = () => (
-  <>
-    <Helmet>
-      <title>Temperature Monitoring Portal</title>
-    </Helmet>
-    <Box
-      sx={{
-        backgroundColor: "background.default",
-        minHeight: "100%",
-        py: 3,
-      }}
-    >
-      <Container maxWidth={false}>
-        <Grid container spacing={3}>
-          <Grid item lg={3} sm={6} xl={3} xs={12}>
-            <Budget />
+const Dashboard = () => {
+  const [cookies, setCookies] = useCookies(["temp_monitoring_auth"]);
+  const navigate = useNavigate();
+
+  React.useEffect(() => {
+    if (cookies.user === undefined) {
+      navigate("/", { replace: true });
+    }
+  });
+
+  return (
+    <>
+      <Helmet>
+        <title>Temperature Monitoring Portal</title>
+      </Helmet>
+      <Box
+        sx={{
+          backgroundColor: "background.default",
+          minHeight: "100%",
+          py: 3,
+        }}
+      >
+        <Container maxWidth={false}>
+          <Grid container spacing={3}>
+            <Grid item lg={3} sm={6} xl={3} xs={12}>
+              <Budget />
+            </Grid>
+            <Grid item lg={3} sm={6} xl={3} xs={12}>
+              <MinTemperatureHits />
+            </Grid>
+            <Grid item lg={3} sm={6} xl={3} xs={12}>
+              <MaxTemperatureHits />
+            </Grid>
+            <Grid item lg={3} sm={6} xl={3} xs={12}>
+              <TotalTempHits sx={{ height: "100%" }} />
+            </Grid>
           </Grid>
-          <Grid item lg={3} sm={6} xl={3} xs={12}>
-            <MinTemperatureHits />
-          </Grid>
-          <Grid item lg={3} sm={6} xl={3} xs={12}>
-            <MaxTemperatureHits />
-          </Grid>
-          <Grid item lg={3} sm={6} xl={3} xs={12}>
-            <TotalTempHits sx={{ height: "100%" }} />
-          </Grid>
-        </Grid>
-      </Container>
-    </Box>
-  </>
-);
+        </Container>
+      </Box>
+    </>
+  );
+};
 
 export default Dashboard;
